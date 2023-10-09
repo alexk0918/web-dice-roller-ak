4d0f5a375401b8c2f80448c856ad8fb3e5a609a0
alexk0918 <95840883+alexk0918@users.noreply.github.com>
2023-10-09 02:03:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const diceValueInput = document.getElementById("diceValue");
 
 // Function to roll the dice and display the result
 function rollDice() {
-    // Make an API call to the /api/rollDice endpoint
+    // Make an API call to server
     fetch('https://node-js-on-azure-amk.azurewebsites.net/')
         .then((response) => response.json())
         .then((data) => {
